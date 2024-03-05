8f2549635e02837f4dc009549f034dc271ce45f0
arswe <abdurrahmansoftw@gmail.com>
2024-03-05 21:35:29 +0600

    AppSamplePageComponent

--- src/app/pages/extra/extra.routing.ts
+++ src/app/pages/extra/extra.routing.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 
-
 // pages
 import { AppIconsComponent } from './icons/icons.component';
 import { AppSamplePageComponent } from './sample-page/sample-page.component';
@@ -9,14 +8,8 @@ export const ExtraRoutes: Routes = [
   {
     path: '',
     children: [
-      {
-        path: 'icons',
-        component: AppIconsComponent,
-      },
-      {
-        path: 'sample-page',
-        component: AppSamplePageComponent,
-      },
+      { path: 'icons', component: AppIconsComponent },
+      { path: 'sample-page', component: AppSamplePageComponent },
     ],
   },
 ];
