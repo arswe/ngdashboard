b604e1f3e70cc1e83c2e295763f257f5f5d22b5f
arswe <abdurrahmansoftw@gmail.com>
2024-03-05 21:34:08 +0600

    structure done with ng dashbord

---
+++ src/app/layouts/full/sidebar/branding.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-branding',
+  template: `
+    <div class="branding">
+      <a href="/" class="no-underline">
+        <h1 class="align-middle m-2">ABDURRAHMAN</h1>
+      </a>
+    </div>
+  `,
+})
+export class BrandingComponent {
+  constructor() {}
+}
