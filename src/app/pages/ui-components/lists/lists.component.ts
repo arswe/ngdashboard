6e30319af2587946ba8ff40d988b7c75cf806872
arswe <abdurrahmansoftw@gmail.com>
2024-03-05 21:36:27 +0600

    list component

--- src/app/pages/ui-components/lists/lists.component.ts
+++ src/app/pages/ui-components/lists/lists.component.ts
@@ -15,18 +15,9 @@ export class AppListsComponent {
   typesOfShoes: string[] = ['Loafers', 'Sneakers'];
 
   folders: Section[] = [
-    {
-      name: 'Photos',
-      updated: new Date('1/1/16'),
-    },
-    {
-      name: 'Recipes',
-      updated: new Date('1/17/16'),
-    },
-    {
-      name: 'Work',
-      updated: new Date('1/28/16'),
-    },
+    { name: 'Photos', updated: new Date('1/1/16') },
+    { name: 'Recipes', updated: new Date('1/17/16') },
+    { name: 'Work', updated: new Date('1/28/16') },
   ];
   notes: Section[] = [
     {
